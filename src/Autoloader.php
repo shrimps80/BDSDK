6491a53e6578d31735c2bf811bed32907e589ec5
shrimp <1732341@qq.com>
2018-06-13 15:14:22 +0800

    SDK

---
+++ src/Autoloader.php
@@ -0,0 +1,28 @@
+<?php
+class Autoloader{
+  
+  /**
+     * 类库自动加载，写死路径，确保不加载其他文件。
+     * @param string $class 对象类名
+     * @return void
+     */
+    public static function autoload($class) {
+        $name = $class;
+        if(false !== strpos($name,'\\')){
+          $name = strstr($class, '\\', true);
+        }
+        
+        $filename = TOP_AUTOLOADER_PATH . $name.".php";
+        if(is_file($filename)) {
+            include $filename;
+            return;
+        }
+        $filename = TOP_AUTOLOADER_PATH . "/lib/" . $name.".php";
+        if(is_file($filename)) {
+            include $filename;
+            return;
+        }
+    }
+}
+spl_autoload_register('Autoloader::autoload');
+?>
